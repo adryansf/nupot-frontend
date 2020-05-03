b8ac306237dd332d2532e1847cd1ad72fb4b16d1
Lucas Miranda <lucas-m@outlook.com>
2020-05-03 11:42:50 -0300

    Refatoração

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,21 +7,18 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   color: ${props => props.theme.secondary};
+  text-align: center;
+  line-height: 1;
 
-  .intro-text {
-    text-align: center;
-    line-height: 1;
-  }
-
-  .intro-text h1 {
+  h1 {
     font-size: 4em;
   }
 
-  .intro-text h2 {
+  h2 {
     font-size: 6em;
   }
 
-  .intro-text p {
+  p {
     font-size: 1.5em;
     display: inline-block;
     padding: 10px;
@@ -34,7 +31,7 @@ export const Container = styled.div`
     font-size: 1rem;
     font-weight: 600;
     color: rgb(255, 255, 255);
-    background-color: #34C759;
+    background-color: #34c759;
     border-radius: 43px;
     margin: 15px 0px 0px;
     padding: 12px 30px;
@@ -43,27 +40,26 @@ export const Container = styled.div`
   }
 
   @media (max-width: 640px) {
-    .intro-text h1 {
+    h1 {
       font-size: 3em;
     }
 
-    .intro-text h2 {
+    h2 {
       font-size: 3.2em;
     }
   }
 
   @media (max-width: 320px) {
-    .intro-text h1 {
+    h1 {
       font-size: 2.3em;
     }
 
-    .intro-text h2 {
+    h2 {
       font-size: 2em;
     }
 
-    .intro-text p {
+    p {
       font-size: 1em;
     }
-  }
   }
 `;
