8bd1e31232a5775796e9c372b9d88c74c93d6d70
Lucas Miranda <lucas-m@outlook.com>
2020-05-07 18:27:10 -0300

    Adicionado pratos a rotas de pratos

--- src/pages/Dishes/index.js
+++ src/pages/Dishes/index.js
@@ -1,24 +1,35 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Dish from './Dish';
 import Ratings from './Ratings';
 import { Container, RelatedContentContainer, RatingsContainer } from './styles';
 
+import api from '../../services/api';
 // placeholder data
-import { fakeDish, fakeRatings } from './constants';
+import { fakeRatings, fakeDish } from './constants';
 
 export default function MyKitchen() {
+  const location = useLocation();
+  console.log(Location);
+  const [dish, setDish] = useState(fakeDish);
+  useEffect(() => {
+    const fetchDish = async () => {
+      const { data } = await api.get(`/dishes/2`);
+      setDish(data);
+    };
+    fetchDish();
+  }, []);
   return (
     <Container>
       <RelatedContentContainer>
         <h1>Veja também</h1>
       </RelatedContentContainer>
       <Dish
-        key={fakeDish.id}
-        name={fakeDish.name}
-        description={fakeDish.description}
-        price={fakeDish.price}
-        image={fakeDish.image}
+        key={dish.id}
+        name={dish.name}
+        description={dish.description}
+        price={dish.price}
+        image={dish.image}
       />
       <RatingsContainer>
         <Ratings ratings={fakeRatings} />
--- src/pages/Dishes/styles.js
+++ src/pages/Dishes/styles.js
@@ -4,6 +4,7 @@ import Paper from '@material-ui/core/Paper';
 export const Container = styled.div`
   display: flex;
   justify-content: space-around;
+  min-height: 84vh;
 `;
 
 export const RatingsContainer = styled(Paper)`
